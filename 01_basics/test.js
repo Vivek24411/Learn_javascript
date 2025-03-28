6a966e86729d713c38fdc88008ae3dddc1685ec4
Vivek <VVP44814@GMAIL.COM>
2025-03-28 11:43:25 +0000

    learn var

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
 console.log("Hitesh")
+const accountId = 12233445
+let accountEmail = "vivek@24411"
+var accountPassword = "vivek8080"
+accountname = "Vivek"
+let accountcode
+
+console.log("yes")
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountcode,accountname])
